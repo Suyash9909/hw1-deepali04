1cc04fc53b9c8d9b0012b0b735952f6950d31737
Deepali Nagwade <nagwade.deepali@gmail.com>
2023-02-12 16:52:49 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -9,9 +9,9 @@ def number():
        This is to review numbers and basic operations.
        """
     # Write the value 4 to the power of 5 and assign it to variable x.
-    x =4**5
+    x = 4 ** 5 
     # Write the value x divided by 3 and assign it to variable y.
-    y =x/3
+    y = x / 3
     return x,y
 
 def strings():
@@ -25,7 +25,7 @@ def strings():
 
     # Repeat variable stevens 7 times and assign it to variable stevens_7.
 
-    stevens_7 = stevens*7
+    stevens_7 = stevens * 7
 
     # What is the length of stevens_7?
 
@@ -33,7 +33,7 @@ def strings():
 
     # Concatenate variable stevens with string " is great" and assign it to variable great.
 
-    great = stevens +  " is great"
+    great = stevens + " is great"
 
     # Replace "great" with "good" in variable great and assign it to a new variable good.
 
@@ -53,22 +53,21 @@ def list_1D():
 
     # Split variable hoboken on a delimiter(comma) into a list of substrings and assign it to a new variable hoboken_list.
 
-    hoboken_list = hoboken.split()
+    hoboken_list = hoboken.split(',')
 
     # Get the first item in the hoboken_list and assign it to a new variable hoboken_first_item.
 
     hoboken_first_item = hoboken_list[0]
 
-    ####
+    # ####
     l=[2,3,4,1,5,6,9,10,15,12,13,-2,-6,0,0]
 
-    # Inplace sort list l (use .sort() ).
+    # # Inplace sort list l (use .sort() ).
     l.sort()
-
 
     # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
 
-    new_l =l[4:11]
+    new_l = l[3:10]
 
     return hoboken,hoboken_list, hoboken_first_item, l, new_l
 
@@ -82,16 +81,15 @@ def list_2D():
 
     # Collect the items in the last column of matrix A using list comprehension and assign it to a new variable last_column.
 
-    last_column = ""
+    last_column = [i[2] for i in A]
 
     # Get the item at the last row and last column of A.
 
-    a = ""
+    a = A[2][2]
 
     # Get the item at row 2 and column 1 of A.
 
-    b =""
-
+    b = A[1][0]
 
     return A,last_column, a, b
 
@@ -104,17 +102,15 @@ def dictionary():
     #   "fruit" => "apple"
     #   "quantity" => 18
     #   "color" => "red"
-    fruit_dict ={
-        "fruits": "apple",
-        "quantity" : 18,
-        "color" : "red"
-    }
+    fruit_dict = {"fruit": "apple", "quantity": 18, "color": "red"}
     
     # Get the item in dictionary fruit_dict that the key "fruit" maps to.
 
-    f = fruit_dict["fruits"]
+    f = fruit_dict["fruit"]
+
     # Increase the value that key "quantity" map to by 1.
 
+    fruit_dict["quantity"] += 1
 
     return fruit_dict, f
 
@@ -124,11 +120,7 @@ def dictionary_nested():
     #   "jobs" => ["scientist", "engineer"] (a list)
     #   "age" => 85
 
-    Grace ={
-        "name"  : { "first_name" : "Grace", "last_name" : "Hopper" },
-        "jobs" : ["scientist", "engineer"],
-        "age"  : 85
-    }
+    Grace = {"name": {"first_name": "Grace", "last_name": "Hopper"}, "jobs": ["scientist", "engineer"], "age": 85}
 
     # Get the value of key "last_name" from the subdictionary of key "name" in dictionary Grace. (aka."Hopper")
 
@@ -136,12 +128,11 @@ def dictionary_nested():
 
     # Add "programmer" to the list that key "jobs" maps to.
 
-    Grace["jobs"].append('programmer')
+    Grace["jobs"].append("programmer")
 
     # Get the third item in the list that key "job" maps to. (the item  you recently added)
     
     job = Grace["jobs"][2]
-
 
 
     return Grace,last_name, job
